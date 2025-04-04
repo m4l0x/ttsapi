551178ca0d7134511dd9d3561d19e4b2516c5b8c
m4l0x <gonzalo.melo@gmail.com>
2025-04-04 20:46:14 -0300

    Del Commit

--- tts-api/routes/tts.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { synthesizeText } = require('../controllers/ttsController');
-
-router.post('/synthesize', synthesizeText);
-
-module.exports = router;
